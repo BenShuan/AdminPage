4ae49d7ccfbdb8938316fd5822b0bae2ae670038
Ben Shuan <benshuan@gmail.com>
2024-04-12 12:14:23 +0300

    fit

---
+++ src/config.jsx
@@ -0,0 +1,24 @@
+
+const REACT_APP_FIREBASE_AUTH_DOMAIN = "chatapp-ee84e.firebaseapp.com"
+const REACT_APP_FIREBASE_DATABASE_URL = "https://chatapp-ee84e-default-rtdb.europe-west1.firebasedatabase.app"
+const REACT_APP_FIREBASE_PROJECT_ID = "chatapp-ee84e"
+const REACT_APP_FIREBASE_STORAGE_BUCKET = "chatapp-ee84e.appspot.com"
+const REACT_APP_FIREBASE_MESSAGING_SENDER_ID = "243279940596"
+const REACT_APP_FIREBASE_APT_KEY = "AIzaSyCfbhjQsqQuTO-0yQrMqvRVtKzjqzQz9us"
+const REACT_APP_FIREBASE_APP_ID = "1:243279940596:web:87a0c370022eb3d5f687a1"
+const REACT_APP_FIREBASE_MEASUREMENT_ID = "G-WNZSCNJV8C"
+export const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_APT_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+export const auth0Config = {
+  client_id: "n1MlKdnWJqdersqOeLorg3WVOwO2BqwR",
+  domain: "dev-fznuz0pisr3h17jn.us.auth0.com"
+};
